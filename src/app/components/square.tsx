d74885d65124527dd6ebcea946ff3d27ea4ee501
samueldebenedictis <samuel.debenedictis@yahoo.it>
2025-07-09 01:39:20 +0200

    chore: add simple stories

--- src/app/components/square.tsx
+++ src/app/components/square.tsx
@@ -4,7 +4,7 @@ type SquareProps = {
 
 export default function Square(props: SquareProps) {
   return (
-    <div className="rounded-xl size-32 border-8 p-4 text-xl">
+    <div className="rounded-xl size-32 border-8 p-4 m-2 text-xl bg-yellow-200 text-black">
       {props.number}
     </div>
   );
