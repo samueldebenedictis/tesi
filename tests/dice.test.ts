83cca33e4a7666704a8b236e217c014abac407dd
samueldebenedictis <samuel.debenedictis@yahoo.it>
2025-07-08 17:23:19 +0200

    chore: add describe

--- tests/dice.test.ts
+++ tests/dice.test.ts
@@ -1,33 +1,35 @@
-import { expect, test, vi } from "vitest";
+import { describe, expect, test, vi } from "vitest";
 import { Dice } from "@/model/dice";
 
 const RANDOM_TEST_NUMBER = 100;
 
-test("Dice build", () => {
-  const dice = new Dice(10);
+describe("Dice", () => {
+  test("Build", () => {
+    const dice = new Dice(10);
 
-  const value = dice.roll();
-  expect(value).toBeLessThanOrEqual(10);
-  expect(value).toBeGreaterThan(0);
-});
-
-test("Dice random roll", () => {
-  const dice = new Dice(6);
-
-  for (let i = 0; i++; i < RANDOM_TEST_NUMBER) {
     const value = dice.roll();
-    expect(value).toBeLessThanOrEqual(6);
+    expect(value).toBeLessThanOrEqual(10);
     expect(value).toBeGreaterThan(0);
-  }
-});
+  });
 
-test("Mocked roll", () => {
-  const dice = new Dice(6);
-  const rollSpy = vi.spyOn(dice, "roll").mockReturnValue(1);
+  test("Random roll", () => {
+    const dice = new Dice(6);
 
-  expect(dice.roll()).toBe(1);
-  expect(dice.roll()).toBe(1);
-  expect(rollSpy).toHaveBeenCalledTimes(2);
+    for (let i = 0; i++; i < RANDOM_TEST_NUMBER) {
+      const value = dice.roll();
+      expect(value).toBeLessThanOrEqual(6);
+      expect(value).toBeGreaterThan(0);
+    }
+  });
 
-  rollSpy.mockRestore();
+  test("Mocked roll", () => {
+    const dice = new Dice(6);
+    const rollSpy = vi.spyOn(dice, "roll").mockReturnValue(1);
+
+    expect(dice.roll()).toBe(1);
+    expect(dice.roll()).toBe(1);
+    expect(rollSpy).toHaveBeenCalledTimes(2);
+
+    rollSpy.mockRestore();
+  });
 });
