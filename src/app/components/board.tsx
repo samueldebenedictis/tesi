dc003a31831b781a289311897c7dda2f2de4b37f
samueldebenedictis <samuel.debenedictis@yahoo.it>
2025-07-08 12:36:16 +0200

    chore: wip board component

--- src/app/components/board.tsx
+++ src/app/components/board.tsx
@@ -4,3 +4,11 @@ type BoardProps = {
   boardsize: number;
   squares: Square[];
 };
+
+export default function Board(props: BoardProps) {
+  return (
+    <div className="rounded-xl size-32 border-8 p-4 text-xl">
+      {props.boardsize}
+    </div>
+  );
+}
