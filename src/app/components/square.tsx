3da28fdab815516f904945c45c1f2bd098b046db
samueldebenedictis <samuel.debenedictis@yahoo.it>
2025-07-09 13:15:01 +0200

    feat: ui wip square

--- src/app/components/square.tsx
+++ src/app/components/square.tsx
@@ -1,11 +1,35 @@
+type Color = "yellow" | "blue" | "green" | "red" | "white";
+
 type SquareProps = {
   number: number;
+  color?: Color;
+};
+
+const squareColor = (color: Color) => {
+  switch (color) {
+    case "yellow":
+      return `bg-linear-to-b from-amber-500 to-yellow-300 text-white outline-2 -outline-offset-4`;
+    case "blue":
+      return `bg-linear-to-b from-sky-500 to-blue-300 text-white outline-2 -outline-offset-4`;
+    case "green":
+      return `bg-linear-to-b from-green-500 to-emerald-300 text-white outline-2 -outline-offset-4`;
+    case "red":
+      return `bg-linear-to-b from-red-500 to-orange-300 text-white outline-2 -outline-offset-4`;
+    default:
+      return "bg-white";
+  }
 };
 
 export default function Square(props: SquareProps) {
+  const colorClass = squareColor(props.color || "white");
+  const classNameBase =
+    "rounded-xl size-32 p-4 m-2 grid grid-cols-1 content-center justify-items-center";
+  const className = [classNameBase, colorClass].join(" ");
   return (
-    <div className="rounded-xl size-32 border-8 p-4 m-2 text-xl bg-yellow-200 text-black">
-      {props.number}
+    <div className={className}>
+      <span className="text-white font-bold text-6xl drop-shadow-xl">
+        {props.number}
+      </span>
     </div>
   );
 }
