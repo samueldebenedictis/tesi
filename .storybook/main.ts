161778434352e072f2e27f18e0f40b82bdc6dd79
samueldebenedictis <samuel.debenedictis@yahoo.it>
2025-07-09 17:46:01 +0200

    chore: move stories

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,10 +1,7 @@
 import type { StorybookConfig } from "@storybook/nextjs";
 
 const config: StorybookConfig = {
-  stories: [
-    "../src/**/*.stories.@(js|jsx|ts|tsx)",
-    "../src/**/stories.@(js|jsx|ts|tsx)",
-  ],
+  stories: ["./**/*.stories.@(js|jsx|ts|tsx)", "./**/stories.@(js|jsx|ts|tsx)"],
   addons: ["@storybook/addon-essentials"],
   framework: "@storybook/nextjs",
   staticDirs: ["../public"],
--- .storybook/square.stories.ts
+++ .storybook/square.stories.ts
@@ -1,5 +1,5 @@
 import type { Meta, StoryObj } from "@storybook/react";
-import Square from "@/app/components/square";
+import Square from "../src/app/components/square";
 
 // More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
 const meta = {
@@ -20,5 +20,7 @@ type Story = StoryObj<typeof meta>;
 export const Primary: Story = {
   args: {
     number: 1,
+    buildingIndex: 0,
+    backgroundColorGradient: "blue",
   },
 };
