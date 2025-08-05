5ad111259687661bb6f4d4d0d135f38d6a14a049
samueldebenedictis <samuel.debenedictis@yahoo.it>
2025-08-05 16:18:36 +0200

    chore: add play turn button

--- src/app/game/page.tsx
+++ src/app/game/page.tsx
@@ -13,14 +13,37 @@ export default function Home() {
     return new Square(i);
   });
 
-  const [game, _setGame] = useState(new Game(squares, ["Renzo", "Lucia"]));
+  const [game, setGame] = useState(new Game(squares, ["Renzo", "Lucia"]));
+
+  // Variabili di gioco
+  // const [nomiGiocatori, setNomiGiocatori] = useState(["", ""]);
+  // const [iconeGiocatori, setIconeGiocatori] = useState(["", ""]);
+
+  // const [caricamentoEffettuato, setCaricamentoEffettuato] = useState(false);
+  const [counter, setCount] = useState(0);
+
+  function onButtonGiocaTurnoClick() {
+    // if (localStorage.getItem("istanzaGioco") == undefined) {
+    //   throw new Error(
+    //     "Impossibile giocare un turno poichè l'istanza corrente non esiste!"
+    //   );
+    // }
+
+    game.playTurn();
+    // setScore(lancioDiDado);
+    setCount(counter + 1);
+    setGame(game);
+    // console.log(giocoAggiornato);
+    localStorage.setItem("COUNTER", `${counter}`);
+    // localStorage.setItem("istanzaGioco", JSON.stringify(giocoAggiornato));
+  }
 
   useEffect(() => {
     localStorage.setItem(GAME_INSTANCE_KEY, JSON.stringify(game));
 
-    const _istanzaGiocoObject = JSON.parse(
-      localStorage.getItem(GAME_INSTANCE_KEY) as string,
-    );
+    // const _istanzaGiocoObject = JSON.parse(
+    //   localStorage.getItem(GAME_INSTANCE_KEY) as string,
+    // );
 
     // console.log(JSON.stringify(game));
     // console.log(JSON.stringify(istanzaGiocoObject));
@@ -28,7 +51,6 @@ export default function Home() {
     // setGame(new Game(squares, ["Renzo", "Lucia"]));
   }, [game]);
 
-  game.playTurn();
   console.log(JSON.stringify(game));
   const squaresC = game
     .getBoard()
@@ -45,11 +67,25 @@ export default function Home() {
     );
   return (
     <ClientOnly>
-      <div className="mx-auto items-center flex flex-col justify-center">
-        {Board({
-          squares: squaresC,
-          cols: 5,
-        })}
+      <div>
+        <div>
+          <button
+            type="button"
+            onClick={() => {
+              // setPlayed(played + 1);
+              onButtonGiocaTurnoClick();
+            }}
+            className="flex h-12 w-full rounded-full bg-black justify-center items-center font-bold text-xl text-yellow-100"
+          >
+            Gioca un turno
+          </button>
+        </div>
+        <div className="mx-auto items-center flex flex-col justify-center">
+          {Board({
+            squares: squaresC,
+            cols: 5,
+          })}
+        </div>
       </div>
     </ClientOnly>
   );
