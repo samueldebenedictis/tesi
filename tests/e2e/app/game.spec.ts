38f459d025fed5fc8ee83b4fd032ca4ff085a0b4
samueldebenedictis <samuel.debenedictis@yahoo.it>
2025-08-06 17:46:36 +0200

    test: add simple test e2e

---
+++ tests/e2e/app/game.spec.ts
@@ -0,0 +1,18 @@
+import { expect, test } from "@playwright/test";
+
+test("Has title", async ({ page }) => {
+  await page.goto("/");
+  await page.getByRole("spinbutton", { name: "Number of Players:" }).fill("3");
+  await page.getByRole("textbox", { name: "Player 1 Name:" }).fill("Qui");
+  await page.getByRole("textbox", { name: "Player 2 Name:" }).fill("Quo");
+  await page.getByRole("textbox", { name: "Player 3 Name:" }).fill("Qua");
+  await page.getByRole("spinbutton", { name: "Number of Squares:" }).fill("25");
+
+  await page.getByRole("button", { name: "Start Game" }).click();
+  await expect(page).toHaveURL(/game/);
+
+  const storageState = await page.context().storageState();
+  expect(
+    JSON.stringify(storageState.origins[0].localStorage, undefined, 2),
+  ).toMatchSnapshot("e2e-config.json");
+});
