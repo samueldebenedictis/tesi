ceca0ebde8e36d3ac2d4143fe0e9b8273a6c5c95
samueldebenedictis <samuel.debenedictis@yahoo.it>
2025-08-23 18:46:03 +0200

    feat: add header link

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -1,20 +1,27 @@
 "use client";
 
 import Link from "next/link";
+import { URL_GAME, URL_HOME } from "../vars";
 import { colorToCss } from "./color";
+
+function HeaderLink(props: { text: string; url: string }) {
+  return (
+    <Link
+      href={props.url}
+      prefetch={false}
+      className="transition-all duration-200 hover:scale-110 hover:underline font-londrina-solid text-xl text-gray-200"
+    >
+      {props.text}
+    </Link>
+  );
+}
 
 export default function Header() {
   return (
     <header className={`sticky top-0 z-10 ${colorToCss("yellow")}`}>
       <nav className="mx-auto flex max-h-20 max-w-7xl items-center justify-between p-6 lg:px-8">
-        <Link
-          href="/"
-          prefetch={false}
-          className="transition-all duration-200 hover:scale-110 hover:underline font-londrina-solid text-xl text-gray-200"
-        >
-          {" "}
-          HOME
-        </Link>
+        <HeaderLink text="HOME" url={URL_HOME} />
+        <HeaderLink text="VAI AL GIOCO" url={URL_GAME} />
       </nav>
     </header>
   );
