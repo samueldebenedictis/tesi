d9cc8bfc448f7bb895652bc7a2855f177bf18593
samueldebenedictis <samuel.debenedictis@yahoo.it>
2025-09-04 00:10:13 +0200

    feat: add slider for special squares percentage

--- src/app/texts.ts
+++ src/app/texts.ts
@@ -6,6 +6,8 @@ export const LABEL_PLAYER_NAME = (n: number) => `Nome giocatore ${n}:`;
 export const LABEL_SQUARES_NUMBER = "Numero di caselle:";
 export const LABEL_GAME_CONFIGURATION = "Configurazione gioco";
 export const LABEL_SPECIAL_SQUARES = "Caselle speciali";
+export const LABEL_SPECIAL_PERCENTAGE = (n: number) =>
+  `Percentuale di caselle speciali: ${n}%`;
 export const LABEL_MIME = "Caselle mimo";
 export const LABEL_QUIZ = "Caselle quiz";
 export const LABEL_MOVE = "Caselle movimento";
