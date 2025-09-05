e8074359ee8edab916ee9dcaec1696fb2c7d2f23
samueldebenedictis <samuel.debenedictis@yahoo.it>
2025-09-05 23:28:51 +0200

    feat: update stories

--- .storybook/pawn.stories.ts
+++ .storybook/pawn.stories.ts
@@ -1,26 +1,46 @@
 import type { Meta, StoryObj } from "@storybook/react";
 import Pawn from "../src/app/components/pawn";
 
-// More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
 const meta = {
   title: "Example/Pawn",
   component: Pawn,
   parameters: {
-    // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/configure/story-layout
     layout: "centered",
   },
-  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
   tags: ["autodocs"],
 } satisfies Meta<typeof Pawn>;
 
 export default meta;
 type Story = StoryObj<typeof meta>;
 
-// More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
-export const SinglePlayer: Story = {
+export const YellowPawn: Story = {
   args: {
     color: "yellow",
     name: "Samuel",
+    isCurrentPlayerTurn: false,
+  },
+};
+
+export const BluePawn: Story = {
+  args: {
+    color: "blue",
+    name: "Mario",
+    isCurrentPlayerTurn: false,
+  },
+};
+
+export const RedPawn: Story = {
+  args: {
+    color: "red",
+    name: "Giulia",
+    isCurrentPlayerTurn: false,
+  },
+};
+
+export const GreenPawn: Story = {
+  args: {
+    color: "green",
+    name: "Luca",
     isCurrentPlayerTurn: false,
   },
 };
@@ -37,6 +57,14 @@ export const StackedPlayers: Story = {
   args: {
     color: "yellow",
     name: "2 giocatori",
+    isCurrentPlayerTurn: false,
+  },
+};
+
+export const LongName: Story = {
+  args: {
+    color: "blue",
+    name: "Giocatore con nome molto lungo",
     isCurrentPlayerTurn: false,
   },
 };
