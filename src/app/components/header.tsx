d2200fe1c2202d785548f1b32d46980711f57510
samueldebenedictis <samuel.debenedictis@yahoo.it>
2025-09-09 23:29:56 +0200

    feat: link instructions refactor

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -1,31 +1,44 @@
 "use client";
 
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import { URL_INSTRUCTION } from "../vars";
 import { colorToCss } from "./ui/color";
 import DropdownMenu from "./ui/dropdown-menu";
 
+const headerTextClasses =
+  "ui-text-title ui-animation-scale ui-text-light hover:underline";
+
 function HeaderLink(props: { text: string; url: string }) {
   return (
-    <Link
-      href={props.url}
-      prefetch={false}
-      className="ui-text-title ui-animation-scale ui-text-light hover:underline"
-    >
+    <Link href={props.url} prefetch={false} className={headerTextClasses}>
       {props.text}
     </Link>
   );
 }
 
 export default function Header() {
+  const pathname = usePathname();
+
+  const handleBack = () => {
+    window.history.back();
+  };
+
   return (
     <header className={`sticky top-0 z-10 ${colorToCss("blue")}`}>
       <nav className="mx-auto flex max-h-20 max-w-sm items-center justify-between p-8">
         <DropdownMenu />
-        {/* <HeaderLink text="NUOVA PARTITA" url={URL_HOME} />
-        <HeaderLink text="CONTINUA PARTITA" url={URL_GAME} />
-        <HeaderLink text="CARICA PARTITA" url={URL_RESTORE_GAME} /> */}
-        <HeaderLink text="ISTRUZIONI" url={URL_INSTRUCTION} />
+        {pathname === "/instructions" ? (
+          <button
+            type="button"
+            onClick={handleBack}
+            className={headerTextClasses}
+          >
+            INDIETRO
+          </button>
+        ) : (
+          <HeaderLink text="ISTRUZIONI" url={URL_INSTRUCTION} />
+        )}
       </nav>
     </header>
   );
