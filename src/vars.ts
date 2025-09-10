c8d748c47add3e9dde08551ff5c9bb4ffeca0faa
samueldebenedictis <samuel.debenedictis@yahoo.it>
2025-09-10 22:13:22 +0200

    chore: move vars

--- src/vars.ts
+++ src/vars.ts
@@ -9,3 +9,8 @@ export const URL_RESTORE_GAME = "/restore-game";
 
 export const MIN_PLAYERS: number = 2;
 export const MAX_PLAYERS: number = 10;
+export const DEFAULT_PLAYERS: number = 2;
+export const MIN_SQUARES: number = 10;
+export const MAX_SQUARES: number = 100;
+export const DEFAULT_SQUARES: number = 30;
+export const DEFAULT_SPECIAL_PERCENTAGE_SQUARES: number = 20;
