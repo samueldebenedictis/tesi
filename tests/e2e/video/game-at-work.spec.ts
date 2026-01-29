ff6934eeef3957d306df70711e64153307d81c1c
samueldebenedictis <samuel.debenedictis@yahoo.it>
2026-01-29 19:58:44 +0100

    chore: add test for video setup

--- tests/e2e/video/game-at-work.spec.ts
+++ tests/e2e/video/game-at-work.spec.ts
@@ -1,27 +1,34 @@
-import type { Page } from "@playwright/test";
-import { test } from "../app/fixtures";
+import { expect, test } from "../app/fixtures";
 
-async function actionWithTimeout(page: Page, action: () => Promise<void>) {
-  await action();
-  await page.waitForTimeout(1000);
-}
+const TIMEOUT = 500;
 
 test("App at work - Setup", async ({ homePage }) => {
-  await actionWithTimeout(homePage.page, () => homePage.playersNumber.clear());
-  await actionWithTimeout(homePage.page, () =>
-    homePage.playersNumber.pressSequentially("3"),
-  );
-  await actionWithTimeout(homePage.page, () =>
-    homePage.playerName(1).pressSequentially("Qui"),
-  );
-  await actionWithTimeout(homePage.page, () =>
-    homePage.playerName(2).pressSequentially("Quo"),
-  );
-  await actionWithTimeout(homePage.page, () =>
-    homePage.playerName(3).pressSequentially("Qua"),
-  );
-  await actionWithTimeout(homePage.page, () => homePage.squaresNumber.clear());
-  await actionWithTimeout(homePage.page, () =>
-    homePage.squaresNumber.pressSequentially("25"),
-  );
+  await homePage.page.waitForTimeout(TIMEOUT);
+  await homePage.playersNumber.fill("3");
+  await homePage.page.waitForTimeout(TIMEOUT);
+  await homePage.playerName(1).fill("Qui");
+  await homePage.page.waitForTimeout(TIMEOUT);
+  await homePage.playerName(2).fill("Quo");
+  await homePage.page.waitForTimeout(TIMEOUT);
+  await homePage.playerName(3).fill("Qua");
+  await homePage.page.waitForTimeout(TIMEOUT);
+  await homePage.squaresNumber.fill("25");
+  await homePage.page.waitForTimeout(TIMEOUT);
+
+  await homePage.mimeCheckbox.uncheck();
+  await homePage.quizCheckbox.uncheck();
+  await homePage.moveCheckbox.uncheck();
+  await homePage.backwriteCheckbox.uncheck();
+  await homePage.musicEmotionCheckbox.uncheck();
+  await homePage.physicalTestCheckbox.uncheck();
+  await homePage.whatWouldYouDoCheckbox.uncheck();
+  await homePage.dictationDrawCheckbox.uncheck();
+  await homePage.faceEmotionsCheckbox.uncheck();
+  await homePage.page.waitForTimeout(TIMEOUT);
+
+  await homePage.submit.click();
+  await homePage.page.waitForTimeout(TIMEOUT);
+
+  await expect(homePage.page).toHaveURL(/game/);
+  await homePage.page.waitForTimeout(TIMEOUT);
 });
