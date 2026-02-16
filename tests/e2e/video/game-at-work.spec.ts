ff2b0f5472780ed3be41aa0d8a001bc628c9cfd6
samueldebenedictis <samuel.debenedictis@yahoo.it>
2026-02-16 19:25:37 +0100

    test: add play turn

--- tests/e2e/video/game-at-work.spec.ts
+++ tests/e2e/video/game-at-work.spec.ts
@@ -91,7 +91,7 @@ test("end game", async ({ page }) => {
   const gamePage = new GamePage(page);
   await addZustandInitScript(gamePage.page, gameData);
   await page.goto("/tesi/game");
-  await gamePage.page.waitForTimeout(2000);
+  await gamePage.page.waitForTimeout(2 * TIMEOUT);
 
   await gamePage.playTurnButton.click();
   await gamePage.page.waitForTimeout(TIMEOUT);
@@ -106,6 +106,35 @@ test("end game", async ({ page }) => {
 
   await gamePage.page.waitForTimeout(TIMEOUT);
 
+  await gamePage.continueButton.click();
+  await gamePage.page.waitForTimeout(TIMEOUT);
+});
+
+test("play turn", async ({ page }) => {
+  const squares = Array.from({ length: 30 }, (_, i) => new Square(i));
+  const players = ["Alice", "Bob"].map((name, i) => new Player(i, name));
+  const board = new Board(squares, players);
+  const game = new Game(board);
+
+  const gameData = game.toJSON();
+  gameData.board.playersPosition[0].position = 8;
+  gameData.board.playersPosition[1].position = 1;
+
+  const gamePage = new GamePage(page);
+  await addZustandInitScript(gamePage.page, gameData);
+  await page.goto("/tesi/game");
+  await gamePage.page.waitForTimeout(2 * TIMEOUT);
+
+  await gamePage.playTurnButton.click();
+  await gamePage.page.waitForTimeout(TIMEOUT);
+
+  await gamePage.rollDiceButton.waitFor();
+
+  await gamePage.rollDiceButton.click();
+  await gamePage.page.waitForTimeout(TIMEOUT);
+
+  await gamePage.continueButton.waitFor();
+  await gamePage.page.waitForTimeout(TIMEOUT);
   await gamePage.continueButton.click();
   await gamePage.page.waitForTimeout(TIMEOUT);
 });
