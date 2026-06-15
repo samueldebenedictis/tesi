b18798bc491f43898d621678e1a448b35795d9c5
samueldebenedictis <samuel.debenedictis@yahoo.it>
2026-06-16 01:06:41 +0200

    chore: update next.config

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,5 @@
 import type { NextConfig } from "next";
 
-const isProd = process.env.NODE_ENV === "production";
-
-const nextConfig: NextConfig = {
-  basePath: isProd ? "/tesi" : "",
-  assetPrefix: isProd ? "/tesi/" : "",
-  images: {
-    unoptimized: true,
-  },
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
